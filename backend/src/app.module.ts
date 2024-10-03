dfea5084ae45ce1d7c0c8faae59d5060f6eaef4c
Nathan Sellar <nathansellar15@gmail.com>
2024-10-04 01:03:00 +1300

    Create: article suggestion form
    adds new page for users to submit their article suggestions to SPEED.  page contains a form to input values like article title, authors etc. The project establishes a connection to the MongoDB database cluster and POSTs the article information to "speedDB" > "articles" collection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -11,18 +11,10 @@ import { ConfigModule } from '@nestjs/config';
 // })
 // export class AppModule {}
 
-// remember to replace the <username> and <password> with your credentials
-const DB_URI = 
-'mongodb+srv://tyn6768:kJLpKFQHKVtmOyEz@cluster0.ic2irq9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-// 'mongodb+srv://jacob0512:hunao512@cluster0.z77u7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-// DB_URI = 'mongodb+srv://tyn6768:kJLpKFQHKVtmOyEz@cluster0.ic2irq9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-
-// const DB_URI =
-  // 'mongodb+srv://<username>:<password>@cluster0.ktxx5bo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 @Module({
-  imports: [MongooseModule.forRoot(DB_URI)],
+  imports: [MongooseModule.forRoot(process.env.DB_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
