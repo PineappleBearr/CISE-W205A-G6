a64ef09c0d89d9492a3cd6ad3772ab3be835dec0
doublemcdingus <srx2318@autuni.ac.nz>
2024-10-17 19:32:57 +1300

    user story 5 done
    the article submission feedback page (eg accepted, rejected, submitted)

---
+++ frontend/src/app/status/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import '../globals.css'
+import SeeArticles from '@/components/ArticleStatus';
+
+export default function status() {
+    return (
+        <main>
+            <SeeArticles />
+        </main>
+    );
+}
